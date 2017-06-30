ff1227c7b7501967d1ecb6b00a5a34ca4a9237c3
gwlim <alphasparc@gmail.com>
2017-06-30 19:25:54 +0800

    Remove AICCU

--- patch_LEDE.sh
+++ patch_LEDE.sh
@@ -14,9 +14,13 @@ sed -i '$d' feeds.conf.default
 ./scripts/feeds install -a
 echo Remove Support for PPPOA
 rm ./feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_pppoa.lua
+echo Remove AICCU (Obsolete)
+rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/network/proto_aiccu.luasrc
+rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/cbi/admin_network/proto_aiccu.lua
 echo Remove Support for DIR-825 and AllNet Devices
 rm ./target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
 rm ./target/linux/ar71xx/base-files/lib/upgrade/allnet.sh
+rm ./target/linux/ar71xx/base-files/lib/upgrade/merakinand.sh
         for i in $( ls patch ); do
             echo Applying patch $i
             patch -p1 < patch/$i
