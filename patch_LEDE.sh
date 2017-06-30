8135c2dab72cfd92f26dbdb8255b16baeca9d37b
gwlim <alphasparc@gmail.com>
2017-06-30 19:37:27 +0800

    Cannot use () in echo

--- patch_LEDE.sh
+++ patch_LEDE.sh
@@ -14,7 +14,7 @@ sed -i '$d' feeds.conf.default
 ./scripts/feeds install -a
 echo Remove Support for PPPOA
 rm ./feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_pppoa.lua
-echo Remove AICCU (Obsolete)
+echo Remove AICCU Obsolete
 rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/network/proto_aiccu.luasrc
 rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/cbi/admin_network/proto_aiccu.lua
 echo Remove Support for DIR-825 and AllNet Devices
