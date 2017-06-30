8ab180b0c139d8590206cb0a20cdd1a930098231
gwlim <alphasparc@gmail.com>
2017-06-30 19:41:30 +0800

    Fix typo

--- patch_LEDE.sh
+++ patch_LEDE.sh
@@ -15,7 +15,7 @@ sed -i '$d' feeds.conf.default
 echo Remove Support for PPPOA
 rm ./feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_pppoa.lua
 echo Remove AICCU Obsolete
-rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/network/proto_aiccu.luasrc
+rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/network/proto_aiccu.lua
 rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/cbi/admin_network/proto_aiccu.lua
 echo Remove Support for DIR-825 and AllNet Devices
 rm ./target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
